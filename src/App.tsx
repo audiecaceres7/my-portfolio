ffb635e775c1157894cb1c1e1dc7982d18714f90
Audie Caceres <audiecaceres@Audies-MacBook-Pro.local>
2023-07-22 15:59:15 -0400

    add hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import "./styles/App.css";
 import NavBar from "./NavBar";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import AnimatedRoutes from "./Routes";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="container">
         <NavBar />
         <AnimatedRoutes />
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
